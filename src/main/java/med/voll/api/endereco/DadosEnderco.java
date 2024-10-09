596a38f8ae8e5d0c6fc1d4f17f7ed1a5bbdbe868
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2024-10-09 15:14:29 -0300

    Validação com Bean Validation.

--- src/main/java/med/voll/api/endereco/DadosEnderco.java
+++ src/main/java/med/voll/api/endereco/DadosEnderco.java
@@ -1,10 +1,20 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEnderco(
+        @NotBlank
         String logradouro,
+        @NotBlank
         String bairro,
+        @NotBlank
+        @Pattern(regexp ="\\d{8}")
         String cep,
+        @NotBlank
         String cidade,
-        String uf, String complemento,
+        @NotBlank
+        String uf,
+        String complemento,
         String numero) {
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedicos.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedicos.java
@@ -1,11 +1,23 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEnderco;
 
 public record DadosCadastroMedicos(
+
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+        @NotNull
         Especialidade especialidade,
-        DadosEnderco endereco) {
+        @NotNull @Valid DadosEnderco endereco) {
 }
