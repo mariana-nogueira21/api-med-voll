9c0effd541396f532643b46b255f59d029830081
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2025-06-09 12:13:20 -0300

    Correção de bugs, nome do parametro diferente da quer e validação quando não tem médico disposivel da data

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -3,12 +3,16 @@ package med.voll.api.domain.consulta.validacoes;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class ValidadorMedicoAtivo {
+@Component
+public class ValidadorMedicoAtivo implements ValidadorAgendamentoDeConsulta {
 
+    @Autowired
     private MedicoRepository repository;
 
     public void validar(DadosAgendamentoConsulta dados) {
